6211a77f9bcc913e3d71a5f6eb773911e2d70edd
younesdiouri <younesdiouri@gmail.com>
2017-03-09 15:43:40 +0100

    plugin bootsnip homeplage

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="/js/shop.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
     <link rel="stylesheet" href="/css/style.css">
--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -9,36 +9,52 @@
 @endsection
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                @foreach ($products as $product)
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,700&subset=latin-ext" rel="stylesheet">
 
-                    <div class="col-sm-6 col-md-4">
-                        <div class="thumbnail" >
-                            <img src="{{$product->imageurl}}" class="img-responsive">
-                            <div class="caption">
-                                <div class="row">
-                                    <div class="col-md-6 col-xs-6">
-                                        <h3>{{$product->name}}</h3>
-                                    </div>
-                                    <div class="col-md-6 col-xs-6 price">
-                                        <h3>
-                                            <label>${{$product->price}}</label></h3>
-                                    </div>
-                                </div>
-                                <p>{{$product->description}}</p>
-                                <div class="row">
-                                    <div class="col-md-6 col-md-offset-3">
-                                        <a href="/addProduct/{{$product->id}}" class="btn btn-success btn-product"><span class="fa fa-shopping-cart"></span> Buy</a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
+
+
+    <!--Item slider text-->
+    <div class="container">
+        <div class="row" id="slider-text">
+            <div class="col-md-6" >
+                <h2>NOUVELLE COLLECTION</h2>
             </div>
         </div>
     </div>
+
+    <!-- Item slider-->
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="carousel carousel-showmanymoveone slide" id="itemslider">
+                    <div class="carousel-inner">
+
+                @foreach ($products as $product)
+
+                            <div class="item active">
+                                <div class="col-xs-12 col-sm-6 col-md-2">
+                                    <a href="#"> <img src="{{$product->imageurl}}" class="img-responsive center-block"></a>
+                                    <h4 class="text-center">{{$product->name}}</h4>
+                                    <h5 class="text-center"><label>${{$product->price}}</label></h5>
+                                    <h6 class="text-center">{{$product->description}}</h6>
+                                    <a href="/addProduct/{{$product->id}}" class="btn btn-success btn-product"><span class="fa fa-shopping-cart"></span> Buy</a>
+                                </div>
+
+                            </div>
+                        @endforeach
+                    <div id="slider-control">
+                        <a class="left carousel-control" href="#itemslider" data-slide="prev"><img src="https://s12.postimg.org/uj3ffq90d/arrow_left.png" alt="Left" class="img-responsive"></a>
+                        <a class="right carousel-control" href="#itemslider" data-slide="next"><img src="https://s12.postimg.org/djuh0gxst/arrow_right.png" alt="Right" class="img-responsive"></a>
+                    </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+            </div>
+        </div>
+
 
 @endsection
 
